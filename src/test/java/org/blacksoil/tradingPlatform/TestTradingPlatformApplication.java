03ec9d859b18217d61f267faf23510eb4c6016b5
Ozsfag <Ozsfag3154artem@gmail.com>
2026-01-05 12:32:41 +0900

    init commit

---
+++ src/test/java/org/blacksoil/tradingPlatform/TestTradingPlatformApplication.java
@@ -0,0 +1,11 @@
+package org.blacksoil.tradingPlatform;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestTradingPlatformApplication {
+
+	static void main(String[] args) {
+		SpringApplication.from(TradingPlatformApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
